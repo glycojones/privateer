cae665096d3287a81f426fd53169a4c0a3ab844c
Jon Agirre <jon.agirre@york.ac.uk>
2016-01-29 16:55:16 +0000

    WIP - start producing SVG glycosylation diagrams

--- privateer-lib.h
+++ privateer-lib.h
@@ -41,6 +41,92 @@ namespace privateer
     
     clipper::ftype real_space_correlation ( const clipper::Xmap<float>&, const clipper::Xmap<float>& );
 
+    
+    namespace glycoplot
+    {
+        
+        class Shape
+        {
+            public:
+                Shape() { } //!< null constructor
+                Shape( int x, int y ) { pos_x = x; pos_y = y; } //!< constructor
+                void set_pos( int x, int y ) { pos_x = x; pos_y =y ; }
+                int  get_y  ( ) { return pos_y; }
+                int  get_x  ( ) { return pos_x; }
+                void set_tooltip ( std::string tooltip ) { this->tooltip = tooltip; }
+            
+            private:
+                int pos_x;
+                int pos_y;
+                std::string tooltip;
+        };
+        
+        class Plot
+        {
+            public:
+                Plot() {} //!< null constructor
+                Plot( int width, int height, bool vertical ) { this->width=width; this->height=height; this->vertical=vertical; }
+                bool plot_to_file ( std::fstream& ); //!< returns true if there have been any problems
+            private:
+                int width;
+                int height;
+                bool vertical;
+                bool write_svg_header   ( std::fstream& );
+                bool write_svg_contents ( std::fstream& );
+                bool write_svg_footer   ( std::fstream& );
+                
+                std::vector < Shape > list_of_shapes;
+                
+        };
+        
+        class Square : public Shape
+        {
+            public:
+                Square() { } //!< null constructor
+                Square( int x, int y, int width, int height ) { set_pos(x, y); this->width=width; this->height=height; } //!< constructor
+                int  get_width  ( ) { return width;  }
+                int  get_height ( ) { return height; }
+                std::string paint ( );
+            
+            private:
+                int width;
+                int height;
+                std::string colour_fill;
+                std::string colour_border;
+                int width_border;
+        };
+        
+        class Circle : public Shape
+        {
+            public:
+                Circle() { } //!< null constructor
+                Circle( int x, int y, int radius ) { set_pos(x, y); this->radius=radius; } //!< constructor
+                int  get_radius  ( ) { return radius;  }
+                std::string paint ( );
+                
+            private:
+                int radius;
+                std::string colour_fill;
+                std::string colour_border;
+                int width_border;
+        };
+        
+        class Triangle : public Shape
+        {
+            public:
+                Triangle() { } //!< null constructor
+                Triangle( int x, int y, int side ) { set_pos(x, y); this->side=side; } //!< constructor
+                int get_side  ( ) { return side;  }
+                std::string paint ( );
+                
+            private:
+                int side;
+                std::string colour_fill;
+                std::string colour_border;
+                int width_border;
+        };
+    }
+    
 } // namespace privateer
 
 #endif
