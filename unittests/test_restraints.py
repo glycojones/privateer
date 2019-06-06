333277d871d98fa8702cc03dac9d862ea9b4aa0b
Jon Agirre <jon.agirre@york.ac.uk>
2019-06-06 22:27:19 +0100

    Added tests for monlib access function

---
+++ unittests/test_restraints.py
@@ -0,0 +1,18 @@
+import os
+import shutil
+import sys
+import privateer.restraints
+
+def test_svg_graphics ( ):
+
+    '''
+    Test restraint and conformer generation
+    '''
+    test_output = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'test_output')
+    if not os.path.exists ( test_output ) : os.makedirs ( test_output )
+
+    print ("Testing access to the CCP4 monomer library")
+
+    access = privateer.restraints.check_monlib_access ( )
+
+    assert ( access )
