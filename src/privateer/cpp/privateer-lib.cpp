ce63b257eca9bc6ad1ec362aee834ab9fc93b519
GABRAH <hb1115@york.ac.uk>
2021-06-16 07:06:59 +0100

    Brought the default lookup of privateer_database.json from master branch

--- src/privateer/cpp/privateer-lib.cpp
+++ src/privateer/cpp/privateer-lib.cpp
@@ -857,11 +857,11 @@ clipper::Xmap<float> privateer::util::read_map_file ( std::string mapin )
 nlohmann::json privateer::util::read_json_file ( clipper::String& path, nlohmann::json& jsonContainer )
 {
     std::string path_copy = path;
-    if(path_copy == "nopath" || path_copy.empty())
+    if(path_copy == "nopath" || path_copy.empty()) 
         {
-            std::string env(std::getenv ( "CINCL" ));
+            std::string env(std::getenv ( "CLIBD" ));
 
-            path_copy = env + "/privateer/" + "database.json";
+            path_copy = env + "/privateer_database.json";
         }
 
     std::cout << "Reading " << path_copy << "... done." << std::endl;
