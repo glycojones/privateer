1e3f17e5737081daab401f7a4c3e72864c0d5c5f
Jon Agirre <jonagirre@Jons-Mac-Studio.local>
2022-12-05 20:38:11 +0000

    More build system changes to support Apple Silicon

--- setup.py
+++ setup.py
@@ -56,6 +56,9 @@ class CMakeBuild(build_ext):
             cmake_args += ['-DCMAKE_BUILD_TYPE=' + cfg]
             build_args += ['--', '-j2']
 
+        if platform.system() == "Darwin" :
+            cmake_args += ['-DCMAKE_C_COMPILER=/opt/homebrew/bin/gcc-12', '-DCMAKE_CXX_COMPILER=/opt/homebrew/bin/g++-12']
+
         env = os.environ.copy()
         env['CXXFLAGS'] = '{} -DVERSION_INFO=\\"{}\\"'.format(
             env.get('CXXFLAGS', ''),
