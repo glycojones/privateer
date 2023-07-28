4b83ae435cb90422953297ddec942585bea760c4
Jordan Dialpuri <dialpuri.jordan@gmail.com>
2023-07-28 18:10:23 +0100

    Added torsion angle data file into filesystem

--- src/privateer/cpp/privateer-bind.cpp
+++ src/privateer/cpp/privateer-bind.cpp
@@ -25,60 +25,7 @@ extern "C" std::vector<std::string> read_file(const std::string &file, const std
   clipper::MiniMol mol; 
   gemmi_file.import_minimol(mol);
 
-  // clipper::Spacegroup spacegroup = clipper::Spacegroup(clipper::Spacegroup::TYPE::P1);
-  // clipper::Cell_descr cell_desc = {structure.cell.a, structure.cell.b, structure.cell.c, structure.cell.alpha, structure.cell.beta, structure.cell.gamma};
-  // clipper::Cell cell = clipper::Cell(cell_desc);
-  // clipper::MiniMol mol(spacegroup, cell);
-
-  // std::cout << "Created minimol" << std::endl;
-
-  // for (int m = 0; m < structure.children().size(); m++)
-  // {
-  //   clipper::MModel model;
-  //   gemmi::Model gemmi_model = structure.children()[m];
-
-  //   for (int c = 0; c < gemmi_model.children().size(); c++)
-  //   {
-  //     gemmi::Chain gemmi_chain = gemmi_model.children()[c];
-  //     clipper::MPolymer polymer;
-  //     polymer.set_id(c);
-  //     for (int r = 0; r < gemmi_chain.children().size(); r++)
-  //     {
-  //       gemmi::Residue gemmi_residue = gemmi_chain.children()[r];
-  //       clipper::MMonomer monomer;
-  //       monomer.set_id(r);
-  //       // monomer.set_type(gemmi_residue.entity_type);
-  //       for (int a = 0; a < gemmi_residue.children().size(); a++)
-  //       {
-  //         gemmi::Atom gemmi_atom = gemmi_residue.children()[a];
-  //         clipper::MAtom atom;
-  //         clipper::Coord_orth coordinate = {gemmi_atom.pos.x, gemmi_atom.pos.y, gemmi_atom.pos.z};
-  //         atom.set_coord_orth(coordinate);
-
-  //         atom.set_id(a);
-  //         atom.set_name(gemmi_atom.name);
-  //         atom.set_occupancy(gemmi_atom.occ);
-
-  //         atom.set_element(gemmi_atom.element.name());
-  //         monomer.insert(atom);
-  //       }
-  //       polymer.insert(monomer);
-  //     }
-  //     model.insert(polymer);
-  //   }
-  //   mol.model() = model;
-  // }
-
-  // for (int p = 0; p < mol.model().size(); p++) { 
-  //   for (int m = 0; m < mol.model()[p].size(); m++) { 
-  //     for (int a = 0; a < mol.model()[p][m].size(); a++) { 
-  //       std::cout << mol.model()[p].id() << "\t" << mol.model()[p][m].id() << "\t" << mol.model()[p][m][a].id() << std::endl;
-  //     }
-  //   }
-  // }
-
-  privateer::json::GlobalTorsionZScore torsions_zscore_database;
-  // = privateer::json::read_json_file_for_torsions_zscore_database("/home/jordan/dev/privateer_wasm/privateer/data/linkage_torsions/privateer_torsions_z_score_database.json");
+  privateer::json::GlobalTorsionZScore torsions_zscore_database = privateer::json::read_json_file_for_torsions_zscore_database("privateer_torsions_z_score_database.json");
   const clipper::MAtomNonBond& manb = clipper::MAtomNonBond( mol, 1.0 ); // was 1.0
 
     clipper::MGlycology mgl = clipper::MGlycology(mol, manb, torsions_zscore_database, false);
