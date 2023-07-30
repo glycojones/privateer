3256796cb589db918381efd3dc2829aefb80f444
Jordan Dialpuri <dialpuri.jordan@gmail.com>
2023-07-30 18:14:03 +0100

    class -> className in Loading and submit Updated gitignore Updated README

--- web/privateer/privateer/src/components/Loading.jsx
+++ web/privateer/privateer/src/components/Loading.jsx
@@ -3,7 +3,7 @@ import ClipLoader from "react-spinners/ClipLoader";
 export default function Loading() { 
    
     return (
-        <div class="flex flex-col items-center justify-center px-12 m-12 h-64 border-2 border-gray-300 border-dashed rounded-lg cursor-pointer bg-gray-50 dark:hover:bg-bray-800 dark:bg-gray-700 dark:border-gray-600">
+        <div className="flex flex-col items-center justify-center px-12 m-12 h-64 border-2 border-gray-300 border-dashed rounded-lg cursor-pointer bg-gray-50 dark:hover:bg-bray-800 dark:bg-gray-700 dark:border-gray-600">
             <h3 className="my-6">Validating glycans...</h3>
             <ClipLoader
                 // color={}
