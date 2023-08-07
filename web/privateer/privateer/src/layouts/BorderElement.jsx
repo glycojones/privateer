b1dd571c6a13eace99ed0f6ed68071351b964d2d
Jordan Dialpuri <dialpuri.jordan@gmail.com>
2023-08-07 13:10:02 +0100

    Add reverse BorderElement

--- web/privateer/privateer/src/layouts/BorderElement.jsx
+++ web/privateer/privateer/src/layouts/BorderElement.jsx
@@ -6,12 +6,16 @@ div {
   }
     `
 
-export default function BorderElement({topColor, bottomColor}) {
+export default function BorderElement({topColor, bottomColor, reverse}) {
 
     const divStyle = {
         height: "90px",
         backgroundImage: `linear-gradient(to bottom right, ${topColor}, ${topColor} 50%, ${bottomColor} 50%, ${bottomColor})`
       };
+
+    if (reverse) { 
+      divStyle['backgroundImage'] = `linear-gradient(to top right, ${bottomColor}, ${bottomColor} 50%, ${topColor} 50%, ${topColor})`
+    }
 
     return (
         <div style={divStyle}></div>   
