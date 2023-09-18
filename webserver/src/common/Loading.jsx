9dabe2bb0644e91ec209e152a8a6f187e67c639f
Dialpuri <dialpuri.jordan@gmail.com>
2023-09-18 15:10:36 +0100

    Small update to run GitHub actions

--- webserver/src/common/Loading.jsx
+++ webserver/src/common/Loading.jsx
@@ -7,7 +7,6 @@ export default function Loading({loadingText}) {
             className="flex flex-col items-center justify-center px-12 m-12 h-64 min-w-64 border-2 border-gray-300 rounded-lg cursor-pointer bg-gray-50 dark:hover:bg-bray-800 dark:bg-gray-700 dark:border-gray-600">
             <h3 className="my-6">{loadingText}</h3>
             <ClipLoader
-                // color={}
                 loading={true}
                 size={100}
                 aria-label="Loading Spinner"
