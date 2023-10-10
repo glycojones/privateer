77e2e7a0f84fbd401bf60aa23e2088384323473a
dialpuri <dialpuri.jordan@gmail.com>
2023-10-10 18:09:22 +0100

    Added UOY legal to Footer

--- webserver/src/layouts/Footer.jsx
+++ webserver/src/layouts/Footer.jsx
@@ -4,10 +4,15 @@ export default function Footer() {
     return (
         <footer className="bg-gray w-full mt-auto">
             <div className="w-full mx-auto max-w-screen-xl p-4 flex items-center justify-center">
-        <span className="text-sm text-primary text-center dark:text-gray-400">
+
+        <span className="text-sm text-primary text-center justify-center dark:text-gray-400">
             Jordan Dialpuri, Haroldas Bagdonas and  Jon Agirre
             <br/>
             © 2023 University of York. All Rights Reserved.
+            <br/>
+             <a href="https://www.york.ac.uk/about/legal-statements/">Disclaimer</a>
+             <img className="w-48 mx-auto" src="uoy_logo.png"/>
+
         </span>
 
             </div>
