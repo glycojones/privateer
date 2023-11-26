9ca619975b2cee0844e7e4522a7ff593eb1acba5
Jordan Dialpuri <44945647+Dialpuri@users.noreply.github.com>
2023-11-26 15:52:15 +0000

    formatted types

--- webserver/src/interfaces/types.ts
+++ webserver/src/interfaces/types.ts
@@ -1,30 +1,30 @@
 import { Dispatch, SetStateAction } from "react";
 
-export interface TorsionEntry { 
-    sugar_1: string, 
-    sugar_2: string, 
+export interface TorsionEntry {
+    sugar_1: string,
+    sugar_2: string,
     atom_number_1: string
-    atom_number_2: string, 
-    phi: number, 
-    psi: number 
+    atom_number_2: string,
+    phi: number,
+    psi: number
 }
 
-export interface TableDataEntry { 
-    svg: string, 
-    wurcs: string, 
-    chain: string, 
-    glyconnect_id: string, 
+export interface TableDataEntry {
+    svg: string,
+    wurcs: string,
+    chain: string,
+    glyconnect_id: string,
     glytoucan_id: string,
-    id: string, 
-    torsion_err: number, 
-    conformation_err: number, 
-    anomer_err: number, 
-    puckering_err: number, 
-    chirality_err: number, 
+    id: string,
+    torsion_err: number,
+    conformation_err: number,
+    anomer_err: number,
+    puckering_err: number,
+    chirality_err: number,
     torsions: Array<TorsionEntry>
 }
 
-export interface HeaderProps { 
+export interface HeaderProps {
     resetApp: boolean,
     setResetApp: Dispatch<SetStateAction<boolean>>,
     PDBCode: string,
@@ -43,7 +43,7 @@ export interface HeaderProps {
     failureText: string
 }
 
-export interface DatabaseHeaderProps { 
+export interface DatabaseHeaderProps {
     resetApp: boolean,
     setResetApp: Dispatch<SetStateAction<boolean>>,
     PDBCode: string,
@@ -53,46 +53,46 @@ export interface DatabaseHeaderProps {
     loadingText: string,
     fallback: boolean,
     failureText: string,
-    results: any, 
+    results: any,
 }
 
-export interface DatabaseResultProps { 
+export interface DatabaseResultProps {
     results: any
     PDBCode: string
 }
 
 
-export interface SNFGProps extends HeaderProps { 
+export interface SNFGProps extends HeaderProps {
     filename: string
 }
 
-export interface UploadButtonProps { 
+export interface UploadButtonProps {
     setCoordinateFile: Dispatch<SetStateAction<File | null>>,
     setReflectionFile: Dispatch<SetStateAction<File | null>>
 }
 
-export interface GlycanDetailProps { 
-    tableData: Array<TableDataEntry>, 
-    hideMoorhen: boolean, 
+export interface GlycanDetailProps {
+    tableData: Array<TableDataEntry>,
+    hideMoorhen: boolean,
     setHideMoorhen: Dispatch<SetStateAction<boolean>>,
-    rowID: number, 
-    forwardControls: any, 
-    scrollPosition: number, 
-    controls: any, 
-    molecule: any, 
+    rowID: number,
+    forwardControls: any,
+    scrollPosition: number,
+    controls: any,
+    molecule: any,
     map: any
 }
 
-export interface NoGlycansProps { 
-    setResetApp: Dispatch<SetStateAction<boolean>>, 
+export interface NoGlycansProps {
+    setResetApp: Dispatch<SetStateAction<boolean>>,
     text: string
 }
 
-export interface SVGTableProps { 
-    tableData: Array<TableDataEntry>, 
-    allowRowClick: boolean, 
+export interface SVGTableProps {
+    tableData: Array<TableDataEntry>,
+    allowRowClick: boolean,
     rowClick: boolean,
-    setRowClicked: Dispatch<SetStateAction<boolean>>, 
+    setRowClicked: Dispatch<SetStateAction<boolean>>,
     setRowID: Dispatch<SetStateAction<number>>
 }
 
