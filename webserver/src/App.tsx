c7e498b7ef83e36f15d51181055647d124271fdf
Jordan Dialpuri <44945647+Dialpuri@users.noreply.github.com>
2023-12-12 17:51:44 +0000

    Added url update to database

--- webserver/src/App.tsx
+++ webserver/src/App.tsx
@@ -4,21 +4,23 @@ import HomeSection from './pages/Home/HomeSection'
 import DatabaseSection from './pages/DatabaseSection/DatabaseSection'
 
 import PageLoad from './components/Loading/PageLoad'
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, useSearchParams } from "react-router-dom";
 import {
   BrowserRouter as Router,
   Link,
   useLocation
 } from "react-router-dom";
+import APIForwarding from './components/APIComponent/APIForwarding'
 
 function useQuery() {
   const { search } = useLocation();
-
   return useMemo(() => new URLSearchParams(search), [search]);
 }
 
+
 function App() {
   let query = useQuery();
+  const [searchParams, setSearchParams] = useSearchParams();
 
   return (
     <Suspense fallback={<PageLoad />}>
@@ -26,7 +28,9 @@ function App() {
 
         <Routes>
           <Route index path="/" element={<HomeSection />} />
-          <Route path="/database" element={<DatabaseSection query={query}/>} />
+          <Route path="/database" element={<DatabaseSection query={query} setSearchParams={setSearchParams} />} />
+          <Route path="/api" element={<APIForwarding query={query}/>} />
+
         </Routes>
       </div>
     </Suspense>
