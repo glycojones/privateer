15c11ce2c2930385775b7e967149f2123a8a355e
Jordan Dialpuri <44945647+Dialpuri@users.noreply.github.com>
2023-12-15 11:45:11 +0000

    Updated build

--- build.sh
+++ build.sh
@@ -4,21 +4,8 @@ source ~/Development/privateer/emsdk/emsdk_env.sh
 # source ~/dev/privateer_wasm/emsdk/emsdk_env.sh
 
 emcmake cmake .
-emmake make -j 
-
-# cp privateer.js webapp/api/privateer.js
-# cp privateer.wasm webapp/api/privateer.wasm
-# cp privateer.data webapp/api/privateer.data
+emmake make -j
 
 mv privateer.js webapp/src/wasm/privateer.js
 mv privateer.wasm webapp/src/wasm/privateer.wasm
 cp privateer.data webapp/public/privateer.data
-
-
-# mv privateer.js web/privateer/privateer/src/wasm/privateer.js
-# mv privateer.wasm web/privateer/privateer/src/wasm/privateer.wasm
-# mv privateer.data web/privateer/privateer/src/wasm/privateer.data
-
-# cp web/privateer/privateer/src/wasm/privateer.data web/privateer/privateer/public/privateer.data
-# cp web/privateer/privateer/src/wasm/privateer.wasm web/privateer/privateer/public/privateer.wasm
-# cp web/privateer/privateer/src/wasm/privateer.js web/privateer/privateer/public/privateer.js
