60f09d8a1c69fda46a4c4f66e364348d996ff79c
Lucy Schofield <lcs551@york.ac.uk>
2024-05-24 16:15:05 +0100

    Updated test file

--- tests/test_CH-Pi.py
+++ tests/test_CH-Pi.py
@@ -1,5 +1,5 @@
 from privateer import privateer_core as pvtcore
-
+from pprint import pprint
 interactions = pvtcore.GlycosylationInteractions(
     path_to_model_file="test_data/5fji.pdb",
     path_to_output_file="test_data/5fji_hydrogenated.pdb",
@@ -7,7 +7,7 @@ interactions = pvtcore.GlycosylationInteractions(
 
 interactions_summary=interactions.get_all_detected_interactions()
 
-print(interactions_summary)
+#pprint(interactions_summary)
 
 for d in interactions_summary:
     print(d['CH_Pi'])
