c011fdc8ae5c19a061868e9579236f34917e6ff1
Dialpuri <dialpuri.jordan@gmail.com>
2024-06-05 11:17:53 +0100

    Add function to fetch PDB file if CIF file fails
    Introduced a fallback function, fetchPDBFile, that attempts to fetch a PDB file in case fetching the CIF file fails. This function catches networking errors and provides clearer logging to facilitate troubleshooting. It also improves the error handling for existing fetchPDB function.

--- webapp/src/utils/fetch_from_pdb.ts
+++ webapp/src/utils/fetch_from_pdb.ts
@@ -1,8 +1,7 @@
-export async function fetchPDB(PDBCode: string): Promise<string | void> {
-    if (PDBCode == null) {
-        return;
-    }
-    console.log('Fetching PDB ', PDBCode);
+async function fetchPDBFile(PDBCode: string): Promise<string | void> {
+    console.warn(
+        'The CIF file for this PDB could not be found, trying for the PDB'
+    );
     const pdbURL = `https://files.rcsb.org/download/${PDBCode.toUpperCase()}.pdb`;
 
     const file = fetch(pdbURL)
@@ -15,8 +14,37 @@ export async function fetchPDB(PDBCode: string): Promise<string | void> {
         .then(async (file) => {
             return file;
         })
-        .catch(() => {
-            throw new Error('PDB Not Found');
+        .catch(async (error) => {
+            return await Promise.reject(error);
+        });
+    return await file;
+}
+
+export async function fetchPDB(PDBCode: string): Promise<string | void> {
+    if (PDBCode == null) {
+        return;
+    }
+    // first try fetching the cif
+    console.log('Fetching PDB ', PDBCode);
+    const pdbURL = `https://files.rcsb.org/download/${PDBCode.toUpperCase()}.cif`;
+
+    const file = fetch(pdbURL)
+        .then(async (response) => {
+            if (!response.ok) {
+                throw new Error('Network error');
+            }
+            return await response.text();
+        })
+        .then(async (file) => {
+            return file;
+        })
+        .catch(async () => {
+            // if we can't find the cif, try the PDB, if that fails, then report the failure.
+            try {
+                return await fetchPDBFile(PDBCode);
+            } catch (e) {
+                return await Promise.reject(e);
+            }
         });
     return await file;
 }
