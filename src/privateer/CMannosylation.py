211582619aae48abd3d11acb0ccd4fc560cfc948
Lou Holland <lou.holland@york.ac.uk>
2024-06-13 12:42:28 +0100

    debug target search

--- src/privateer/CMannosylation.py
+++ src/privateer/CMannosylation.py
@@ -570,11 +570,9 @@ def find_and_graft_Cglycans(receiverdir,mtzdir,donordir,outputdir,redo,graftedli
         except:
             print("Error checking expression system. Proceeding with grafting regardless of expression system...")
             requestedchains = None
-        with open(f"{pdbcode}_requestedchains.txt", "w") as myfile:
-            for requestedchain in requestedchains:
-                myfile.write(f"{requestedchain}\n")
+
         sequences = grafter._get_sequences_in_receiving_model(receiverpath)
-        targets = get_targets_via_blob_search_and_consensus_sequence(receiverpath, mtzpath, requestedchains, sequences, 0.051)
+        targets = get_targets_via_blob_search_and_consensus_sequence(receiverpath, mtzpath, requestedchains, sequences, 0.08)
         #targets_2 = grafter._get_CMannosylation_targets_via_water_search(receiverpath, sequences) #FLAG: remove water search???
         #for target_1 in targets_1:
         #    for target_2 in targets_2[:]:
